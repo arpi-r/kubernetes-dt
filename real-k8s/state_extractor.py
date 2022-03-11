aa738ef1f3279a3b288c4f4a9cf052f8ed2821c5
Deepti <kalasapura@gmail.com>
2022-03-11 13:34:48 -0600

    add cluster state changes

--- real-k8s/state_extractor.py
+++ real-k8s/state_extractor.py
@@ -1,24 +1,75 @@
+import collections
+import json
 from pick import pick  # install pick using `pip install pick`
 
 from kubernetes import client, config
-from kubernetes.client import configuration
 
-# Configs can be set in Configuration class directly or using helper utility
-contexts, active_context = config.list_kube_config_contexts('/home/deepti/.kube/config')
-if not contexts:
-    print("Cannot find any context in kube-config file.")    
-contexts = [context['name'] for context in contexts]
-active_index = contexts.index(active_context['name'])
-option, _ = pick(contexts, title="Pick the context to load",
-                    default_index=active_index)
-# Configs can be set in Configuration class directly or using helper
-# utility
-config.load_kube_config(context=option)
 
-print("Active host is %s" % configuration.Configuration().host)
+def connect_to_cluster():
+    # Configs can be set in Configuration class directly or using helper utility
+    contexts, active_context = config.list_kube_config_contexts(
+        '/home/deepti/.kube/config')
+    if not contexts:
+        print("Cannot find any context in kube-config file.")
+    contexts = [context['name'] for context in contexts]
+    active_index = contexts.index(active_context['name'])
+    # option, _ = pick(contexts, title="Pick the context to load", default_index=active_index)
+    # Configs can be set in Configuration class directly or using helper
+    # utility
 
-v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_namespaced_service('default')
-for i in ret.items:
-    print(i)
+    option = 'kind-real-k8s'
+    config.load_kube_config(context=option)
+
+
+def get_pods_usage(api):
+    pods = []
+    k8s_pods = api.list_cluster_custom_object(
+        "metrics.k8s.io", "v1beta1", "pods")
+    for stats in k8s_pods['items']:
+        if stats['metadata']['namespace'] == 'default':
+            containers = []
+            for container in stats['containers']:
+                containers.append(container)
+            pod = {
+                'name': stats['metadata']['name'],
+                'containers': containers
+            }
+            pods.append(pod)
+    print(pods)
+    return pods
+
+
+def get_nodes_usage(api):
+    nodes = []
+    k8s_nodes = api.list_cluster_custom_object(
+        "metrics.k8s.io", "v1beta1", "nodes")
+    for stats in k8s_nodes['items']:
+        node = {
+            'name': stats['metadata']['name'],
+            'cpu': stats['usage']['cpu'],
+            'memory': stats['usage']['memory']
+        }
+        nodes.append(node)
+
+    print(nodes)
+    return nodes
+
+
+def get_resource_usage():
+    api = client.CustomObjectsApi()
+    pods = get_pods_usage(api)
+    nodes = get_nodes_usage(api)
+
+    cluster_usage = {
+        'nodes': nodes,
+        'pods': pods
+    }
+
+    with open('./cluster_state.json', 'w') as f:
+        state = json.dumps(cluster_usage, indent=2)
+        f.write(state)
+
+
+if __name__ == '__main__':
+    connect_to_cluster()
+    get_resource_usage()
