4312b035857c1986078615300b902a2b0a433803
Deepti <kalasapura@gmail.com>
2022-04-08 00:46:00 -0500

    add service info and container limits

--- real-k8s/state_extractor.py
+++ real-k8s/state_extractor.py
@@ -23,19 +23,27 @@ def connect_to_cluster():
 
 def get_pods_usage(api):
     pods = []
+    v1 = client.CoreV1Api()
+    list_of_pods = v1.list_namespaced_pod('default', watch=False)
     k8s_pods = api.list_cluster_custom_object(
         "metrics.k8s.io", "v1beta1", "pods")
     for stats in k8s_pods['items']:
         if stats['metadata']['namespace'] == 'default':
             containers = []
             for container in stats['containers']:
-                containers.append(container)
+                for p in list_of_pods.items:
+                    if p.metadata.name == stats['metadata']['name']:
+                        cur_pod = p
+                        for pod_container in cur_pod.spec.containers:
+                            if pod_container.name == container['name']:
+                                container['cpu_limit'] = pod_container.resources.limits['cpu']
+                        containers.append(container)
+
             pod = {
                 'name': stats['metadata']['name'],
                 'containers': containers
             }
             pods.append(pod)
-    print(pods)
     return pods
 
 
@@ -51,19 +59,40 @@ def get_nodes_usage(api):
         }
         nodes.append(node)
 
-    print(nodes)
     return nodes
+
+
+def get_service_info():
+    api = client.AutoscalingV1Api()
+    hpa = api.list_horizontal_pod_autoscaler_for_all_namespaces(watch=False)
+    services = []
+    for scaler in hpa.items:
+        if scaler.metadata.namespace == 'default':
+            annotations = json.loads(
+                scaler.metadata.annotations['autoscaling.alpha.kubernetes.io/behavior'])
+            service = {
+                'name': scaler.metadata.name,
+                'maxPods': scaler.spec.max_replicas,
+                'minPods': scaler.spec.min_replicas,
+                'upscaleThreshold': scaler.spec.target_cpu_utilization_percentage,
+                'downscalePeriod': annotations['ScaleDown']['StabilizationWindowSeconds']
+            }
+            services.append(service)
+    return services
+
 
 def map_nodes_to_pods(pods, nodes):
     v1 = client.CoreV1Api()
     for node in nodes:
         node['pods'] = []
         field_selector = 'spec.nodeName=' + node['name']
-        pods_on_node = v1.list_pod_for_all_namespaces(watch = False, field_selector = field_selector)
+        pods_on_node = v1.list_pod_for_all_namespaces(
+            watch=False, field_selector=field_selector)
         for pod in pods_on_node.items:
             for pod_on_node in pods:
                 if pod.metadata.name == pod_on_node['name']:
                     node['pods'].append(pod_on_node)
+
 
 def get_resource_usage():
     api = client.CustomObjectsApi()
@@ -71,8 +100,10 @@ def get_resource_usage():
     nodes = get_nodes_usage(api)
 
     map_nodes_to_pods(pods, nodes)
+    services = get_service_info()
 
     cluster_usage = {
+        'services': services,
         'nodes': nodes
     }
 
